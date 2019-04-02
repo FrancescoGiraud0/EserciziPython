15c745a40b1a112518013404bbda0abc41fd4b5e
Francesco <francesco.giraudo@itiscuneo.eu>
2019-04-02 18:11:19 +0200

    Update dijkstra.py

--- dijkstra/dijkstra.py
+++ dijkstra/dijkstra.py
@@ -22,7 +22,7 @@ def dijkstra(indexNodoMin, peso, matGrafo, **other):
 
     if len(notVisitedList)==0:
         return distanceList
-        
+    
     for i_nodo in notVisitedList:
         if matGrafo[indexNodoMin][i_nodo]!=0:
             if (matGrafo[indexNodoMin][i_nodo]+peso)<distanceList[i_nodo]:
@@ -30,10 +30,8 @@ def dijkstra(indexNodoMin, peso, matGrafo, **other):
 
     indexNextNode = indexMinimo(distanceList, notVisitedList)
 
-    #print(indexNextNode, distanceList[indexNextNode], distanceList, notVisitedList)
-
     return dijkstra(indexNextNode, distanceList[indexNextNode], matGrafo, distanceList=distanceList, notVisitedList=notVisitedList)
-         #0,1,2,3,4,5,6,7
+
 grafo = [[0,1,4,0,0,0,0,0],
          [1,0,0,0,0,2,0,0],
          [4,0,0,5,0,0,0,0],
@@ -44,4 +42,4 @@ grafo = [[0,1,4,0,0,0,0,0],
          [0,0,0,0,3,0,4,0]]
 
 for i in range(0,len(grafo)):
-    print(i,dijkstra(i,0,grafo))
+    print( "Nodo " + str(i) + ": " + str(dijkstra(i,0,grafo)) )
