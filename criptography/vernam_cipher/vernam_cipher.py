952340c2cccb02c1f78c3ee2797e1eaf0bf714e5
Francesco Giraudo <francesco.giraudo@itiscuneo.eu>
2019-10-31 12:02:05 +0100

    Update vernam_cipher.py
    Creation of alphabet dictionary with dict comprehension

--- criptography/vernam_cipher/vernam_cipher.py
+++ criptography/vernam_cipher/vernam_cipher.py
@@ -3,15 +3,8 @@ Giraudo Francesco
 Vernam cipher in python3.
 """
 
-key_word = "ITISDELPOZZO" # Definition of the key
-
 # Definition of the numeric values to the corresponding letters
-alphabet= {
-      0:"A",  1:"B",  2:"C",  3:"D",  4:"E",
-      5:"F",  6:"G",  7:"H",  8:"I",  9:"L",
-     10:"M", 11:"N", 12:"O", 13:"P", 14:"Q",
-     15:"R", 16:"S", 17:"T", 18:"U", 19:"V",
-     20:"Z"}
+alphabet= {chr(65+k) for k in range(0,26)}
 
 # Creation of a new dictionary where key is the letter and the
 # value is a number for encoding.
@@ -19,7 +12,7 @@ encode_alphabet = {value:key for key, value in  alphabet.items()}
 
 def vernam_encrypting(word):
     # function to encrypt char
-    encrypt_char = lambda a,b: alphabet.get((encode_alphabet.get(a) + encode_alphabet.get(b)) % len(alphabet))
+    encrypt_char = lambda a,b: alphabet.get((encode_alphabet.get(a)+encode_alphabet.get(b)) % len(alphabet))
 
     # word encrypting using list comprehension
     encrypted_chars_list = [encrypt_char(c,key_word[i]) if i<len(key_word) else c for i,c in enumerate(list(word))] 
@@ -29,7 +22,7 @@ def vernam_encrypting(word):
 
 def vernam_decrypting(encrypted_word):
     # function to decrypt char
-    decrypt_char = lambda a,b: alphabet.get( (encode_alphabet.get(a)-encode_alphabet.get(b)+len(alphabet)) % len(alphabet))
+    decrypt_char = lambda a,b: alphabet.get( (encode_alphabet.get(a)-encode_alphabet.get(b)+len(alphabet))%len(alphabet) )
 
     # word decrypting using list comprehension
     decrypted_chars_list = [decrypt_char(c,key_word[i]) if i<len(key_word) else c for i,c in enumerate(list(encrypted_word))]
@@ -37,11 +30,16 @@ def vernam_decrypting(encrypted_word):
     # convertion of list of char to string with .join()
     return "".join(decrypted_chars_list)
 
-word = ""
+key_word = "" # Definition of the key
+word = ""  # Definition of the word encrypt / decrypt
 
 while (not word.isalpha()) and len(word)>=0:
     #input of the word to encrypt
     word = input("Insert the word to encrypt (only letters): ").upper() # to uppercase
+
+while (not key_word.isalpha()) and len(key_word)>=0:
+    #input of the word to encrypt
+    key_word = input("Insert the key word to encrypt the word (only letters): ").upper() # to uppercase
 
 encrypted_word = vernam_encrypting(word)
 print(f"Encrypted word: {encrypted_word}")
